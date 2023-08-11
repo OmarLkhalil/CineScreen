d15da1c197f2f81ea75bcfad2310183fba383691
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2023-08-11 14:48:50 +0300

    Finished video trial

--- feature/details/src/main/java/com/mobilebreakero/details/DetailScreen.kt
+++ feature/details/src/main/java/com/mobilebreakero/details/DetailScreen.kt
@@ -9,64 +9,61 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Lifecycling
 import androidx.navigation.NavController
-import com.mobilebreakero.DetailsViewModel
-import com.mobilebreakero.common.domain.model.VideoItemModel
+import com.mobilebreakero.VideosViewModel
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
-@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 @Composable
 fun DetailsScreen(
     navController: NavController,
     lifecycle: LifecycleOwner,
     movieId: String,
-    viewModel: DetailsViewModel = hiltViewModel(),
+    viewModel: VideosViewModel = hiltViewModel(),
 ) {
 
-    val videoKey by viewModel.videoKey.collectAsState()
+    val url = remember {
+        viewModel.videoKey
+    }
 
-    videoKey?.let { key ->
-        Column(
+    LaunchedEffect(Unit) {
+        viewModel.fetchVideos(movieId)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .verticalScroll(rememberScrollState())
+    ) {
+        AndroidView(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black)
-                .verticalScroll(rememberScrollState())
-        ) {
-            AndroidView(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clip(RoundedCornerShape(16.dp)),
-                factory = { context ->
+                .fillMaxWidth()
+                .padding(8.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            factory = { context ->
 
-                    YouTubePlayerView(
-                        context = context,
-                    ).apply {
-                        lifecycle.lifecycle.addObserver(this)
-                        addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-                            override fun onReady(youTubePlayer: YouTubePlayer) {
-                                if (key.isNotEmpty()) {
-                                    youTubePlayer.loadVideo(key, 0f)
-                                }
-                            }
-                        })
-                    }
+                YouTubePlayerView(
+                    context = context,
+                ).apply {
+                    lifecycle.lifecycle.addObserver(this)
+                    addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                        override fun onReady(youTubePlayer: YouTubePlayer) {
+                            youTubePlayer.loadVideo(url.value, 0f)
+                        }
+                    })
                 }
-            )
-        }
+            }
+        )
     }
 }
